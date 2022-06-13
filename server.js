2fe1482b19f564fa2ebff37bbfbb1638f0a03605
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-13 09:19:56 -0500

    wip: intial setup of server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require('express');
+
+// Initialize the Express App
+const app = express();
+
+// Configure App Settings
+require('dotenv').config();
+
+const { PORT = 4000, MONGODB_URL } = process.env;
+
+// Mount Middleware
+
+// Mount Routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// Tell Express to Listen
+app.listen(PORT, () => {
+    console.log(`Express is listening of port:${PORT}`);
+});
